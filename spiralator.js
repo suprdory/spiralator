955eef15ae038cc4313405b5bdd9f8428ce922ad
nathan <suprdory@gmail.com>
2022-06-14 13:37:16 +0100

    v18

--- spiralator.js
+++ spiralator.js
@@ -512,10 +512,11 @@ function addPointerListeners() {
     }
 }
 function zoomHandler(dW, x, y) {
-    x = x / scl0;
-    y = y / scl0;
+
     xt = (x - xOff) / scl
     yt = (y - yOff) / scl
+    x = x / scl0;
+    y = y / scl0;
 
     // console.log(
     //     'dW',dw,
@@ -989,7 +990,7 @@ function anim() {
     // ctx.fillText('yOff='+Math.round(yOff * 10000) / 10000, 20, uiY + 80)
     // ctx.fillText('xOff='+Math.round(xOff * 10000) / 10000, 20, uiY + 110)
     // ctx.fillText('scl='+Math.round(scl * 10000) / 10000, 20, uiY + 140)
-    ctx.fillText('v17', 10, Y - 15)
+    ctx.fillText('v18', 10, Y - 15)
 
 }
 
