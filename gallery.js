83b602e788b2d0f98c7a3da7cea130b5e3b00071
suprdory <suprdory@gmail.com>
2023-12-17 20:38:58 +0000

    use new api function for multi image meta fetch

--- gallery.js
+++ gallery.js
@@ -1,15 +1,18 @@
 
-function add_image(n) {
-    
+
+function add_image_from_meta(data) {
+
+    // console.log(data)
+
     var elemDiv = document.createElement('div');
     elemDiv.style.cssText =
         'text-align:center;';
     elemDiv.style.width =
-        imageWidth+'px'
+        imageWidth + 'px'
 
     const image = document.createElement('img');
     image.loading = "lazy"
-    image.src = galleryAPIurl + '/get_image?n=' + n;
+    image.src = data.url
     image.width = imageWidth
     image.height = imageWidth
     // image.style.maxWidth="100%";
@@ -29,42 +32,41 @@ function add_image(n) {
     elemID.innerText = "ID";
     elemID.classList.add("name")
 
-    fetch(galleryAPIurl + '/get_meta?n=' + n)
-        .then(response => response.json())
-        .then(data => {
-            elemName.innerText = data.name;
-            elemComment.innerText = data.comment;
-            elemID.innerText=data.id;
 
-            if (elemComment.innerText != '') {
-                console.log(elemComment.innerText)
-                elemDiv.appendChild(elemComment);
-            }
-            if (elemName.innerText != '') {
-                elemDiv.appendChild(elemName);
-            }
-            if (dispID){
-            if (elemID.innerText != '') {
-                var button = document.createElement("BUTTON");
-                button.innerText='Delete '+data.id
-                button.addEventListener('click', () => {
-                    // alert('Oh, you clicked me!')
-                    del_image(data.id);
+    elemName.innerText = data.name;
+    elemComment.innerText = data.comment;
+    elemID.innerText = data.id;
+
+    if (elemComment.innerText != '') {
+        console.log(elemComment.innerText)
+        elemDiv.appendChild(elemComment);
+    }
+    if (elemName.innerText != '') {
+        elemDiv.appendChild(elemName);
+    }
+    if (dispID) {
+        if (elemID.innerText != '') {
+            var button = document.createElement("BUTTON");
+            button.innerText = 'Delete ' + data.id
+            button.addEventListener('click', () => {
+                // alert('Oh, you clicked me!')
+                del_image(data.id);
             })
-                button.classList.add("delButton")
+            button.classList.add("delButton")
 
-                elemDiv.appendChild(elemID);
-                elemDiv.appendChild(button)
-            }}
+            elemDiv.appendChild(elemID);
+            elemDiv.appendChild(button)
+        }
+    }
 
-            // console.log(data)
-        });
+    // console.log(data)
+
 
     document.body.appendChild(elemDiv);
 }
 
-function del_image(id){
-    fetch(galleryAPIurl + '/del_image?id=' + id+'&pw='+pw)
+function del_image(id) {
+    fetch(galleryAPIurl + '/del_image?id=' + id + '&pw=' + pw)
         // .then(response => response.json())
         .then(data => {
             console.log(data)
@@ -76,14 +78,15 @@ const urlParams = new URLSearchParams(queryString);
 const dispID = urlParams.get('id');
 const pw = urlParams.get('pw');
 
-let imageWidth = Math.min(window.innerWidth, 1080,window.innerHeight*0.95)
+let imageWidth = Math.min(window.innerWidth, 1080, window.innerHeight * 0.95)
 document.querySelector(':root').style.setProperty('--nameSize', 10 + 'pt')
 document.querySelector(':root').style.setProperty('--commentSize', 10 + 'pt')
 
-for (let i = 0; i < 100; i++) {
-    add_image(i);
-}
 
-// for (let i = 0; i < 40; i++) {
-//     add_comments(i);
-// }
+fetch(galleryAPIurl + '/get_meta_n?i=0&n=200')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            add_image_from_meta(element)
+        });
+    })
